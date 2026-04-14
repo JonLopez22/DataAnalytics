c0b346b8623b1b8571d84a39b92808b4f7d10953
JonLopez22 <lopez.jay22@yahoo.com>
2026-04-14 15:02:35 -0400

    Add week-2 file

---
+++ week-2/Demo2.sql
@@ -0,0 +1,10 @@
+/*
+Jonathan Lopez
+April 14,2026
+*/
+SHOW databases;
+Use sakila;
+-- display all tables in the sakila database
+show tables;
+describe actor;
+show columns from actor;
---
+++ week-2/Ex1B_first_query.sql
@@ -0,0 +1,3 @@
+/*
+sakila is a database used to collect information about _____ and ______ for use by _____.
+*/
