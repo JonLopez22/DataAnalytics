9da85d335d83c9167f2e281df5cd19f84f55fca9
JonLopez22 <lopez.jay22@yahoo.com>
2026-04-21 15:14:05 -0400

    Add Ex1C query to file

--- week-3/Class_Exercises/sqldemo3.1.sql
+++ week-3/Class_Exercises/sqldemo3.1.sql
@@ -62,3 +62,35 @@ WHERE Region is NOT NULL;
 SELECT CompanyName
 FROM Customers
 WHERE CompanyName LIKE 'A%';
+SELECT OrderID, CustomerID, OrderDate
+FROM Orders
+WHERE OrderDate = '1997-01-01';
+SELECT OrderID, OrderDate
+FROM Orders
+WHERE YEAR(OrderDate) = 1997 AND MONTH(OrderDate) = 6;
+SELECT ProductName, UnitPrice
+FROM Products
+ORDER BY ProductName AND UnitPrice DESC;
+SELECT CompanyName, Country, City
+FROM Customers
+ORDER BY Country ASC, CompanyName ASC;
+SELECT ProductName, UnitPrice
+FROM Products
+order by UnitPrice DESC
+LIMIT 5;
+SELECT ProductName, UnitPrice
+FROM Products
+ORDER BY UnitPrice DESC
+LIMIT 5, 5;
+SELECT DISTINCT Country
+FROM Customers
+ORDER BY Country;
+SELECT distinct Country, City
+from Customers
+order by Country, City;
+select concat(FirstName, ' ', LastName) AS 'Full Name',
+Title
+from Employees;
+select ProductName, UnitPrice AS 'Original Price', UnitPrice*0.90 AS '10% Discount'
+from Products
+Order by ProductName ASC;
