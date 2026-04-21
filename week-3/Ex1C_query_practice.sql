095eeb5493434056f0970970ba65f0bacef09205
JonLopez22 <lopez.jay22@yahoo.com>
2026-04-21 15:14:05 -0400

    Add Ex1C query to file

--- week-3/Ex1C_query_practice.sql
+++ week-3/Ex1C_query_practice.sql
@@ -9,13 +9,29 @@ FROM Products
 WHERE QuantityPerUnit >= 100 
 ORDER BY UnitPrice DESC;
 -- 3. What are the products that we carry where we have at least 100 units on hand? Order them in descending order by price. If two or more have the same price, list those in ascending order by product name.
-
+SELECT ProductID, ProductName, UnitsInStock, UnitPrice
+FROM Products
+WHERE UnitsInStock >= 100
+ORDER BY UnitPrice DESC;
 -- 4. Write a query against the orders table that displays the total number of distinct customers who have placed orders, based on customer ID. Use an alias to label the count calculation as CustomerCount.
-
+SELECT COUNT(DISTINCT CustomerID) AS CustomerCount
+FROM Orders;
 -- 5. Write a query against the orders table that displays the total number of distinct customers who have placed orders, by customer ID, for each country where orders have been shipped. Again, use an alias to label the count as CustomerCount. Order the list by the CustomerCount, largest to smallest.
-
+SELECT ShipCountry, COUNT(DISTINCT CustomerID) AS CustomerCount
+FROM Orders
+GROUP BY ShipCountry
+ORDER BY CustomerCount DESC;
 -- 6. What are the products that we carry where we have less than 25 units on hand, but 1 or more units of them are on order? Write a query that orders them by quantity on order (high to low), then by product name.
-
+SELECT ProductID, ProductName, UnitsInStock, UnitsOnOrder
+FROM Products
+WHERE UnitsInStock < 25 AND UnitsOnOrder >= 1
+ORDER BY UnitsOnOrder DESC, ProductName DESC;
 -- 7. Write a query to list each of the job titles in employees, along with a count of how many employees hold each job title.
-
+SELECT Title, COUNT(EmployeeID) AS EmployeeCount
+FROM Employees
+GROUP BY Title;
 -- 8. What employees have a monthly salary that is between $2000 and $2500? Write a query that orders them by job title.
+SELECT EmployeeID, CONCAT(FirstName, ' ', LastName) AS 'Full Name', Title, Salary
+FROM Employees
+WHERE Salary BETWEEN 2000 AND 2500
+ORDER BY Title ASC;
