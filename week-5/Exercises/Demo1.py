2fbf5b05afc9c13b71e7f86fe109e7b19720fbd7
jonathan lopez <jlopez7@my.yearupunited.org>
2026-05-04 15:31:29 -0400

    Add Demo file

---
+++ week-5/Exercises/Demo1.py
@@ -0,0 +1,15 @@
+'''
+Jonathan Lopez
+5/4/2026
+'''
+
+name = "Jonathan"
+age = "28"
+print(name)
+print(age)
+
+pi = 3.14159
+print(f"The value of pi is {pi:.2f}")
+
+sale = 12345.6785
+print(f"The value of the sale is ${sale:,.2f}")
