efa78eb3992b3fbf83d1e882093ece1f1327025d
JonLopez22 <lopez.jay22@yahoo.com>
2026-05-05 13:56:19 -0400

    save to class notes

--- week-5/Class_Notes/May5th_Notes.py
+++ week-5/Class_Notes/May5th_Notes.py
@@ -17,3 +17,45 @@ print(a < b)
 x = 6
 y = 6
 print(x >= y)
+
+name = "Hello"
+
+print(f"Original string: {name}")
+print(f"First character: {name[0]}")
+
+
+string= "Hello, World!"
+# Acessing the first character
+print(string[0])  # Output: H
+
+# Acessing the last character using negative indexing
+print(string[-1])  # Output: !
+
+# Slicing from index 1 to 4 (excluding 4)
+print(string[1:4])  # Output: ell
+
+# Concatenating
+greeting = "Hello, " + "World!"
+print(greeting)  # Output: Hello, World!
+
+# Repetition
+repeat = "Hello! " * 3
+print(repeat)  # Output: Hello! Hello! Hello!
+
+# Membership
+print("World" in string)  # Output: True
+
+string = "Hello, World!"
+
+# convert to uppercase
+print(string.upper())  # Output: HELLO, WORLD!
+
+# find a substring
+print(string.find("World"))  # Output: 7
+
+#replace a substring
+print(string.replace("World", "Python"))  # Output: Hello, Python!
+
+# split a string 
+print(string.split(", "))  # Output: ['Hello', 'World!']
+
