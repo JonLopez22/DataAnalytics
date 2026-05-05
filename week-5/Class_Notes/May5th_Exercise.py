83793e6016ccfd552c307d882d5099b5760f2a80
JonLopez22 <lopez.jay22@yahoo.com>
2026-05-05 14:33:19 -0400

    saves to class notes

---
+++ week-5/Class_Notes/May5th_Exercise.py
@@ -0,0 +1,6 @@
+name1 = "Chantal Lee"
+name2 = "Dimitri Nji"
+name3 = "Vesna Cari"
+print(f"Hello, {name1}! "f"Hello, {name2}! "f"Hello, {name3}! ")
+# Could have used print(f"Hello, {name1}! /n Hello, {name2}! /n Hello, {name3}! ")
+print(f"Hello, {name1}!\nHello, {name2}!\nHello, {name3}!")
