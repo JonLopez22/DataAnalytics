eed3852673b28158a5e79a1e04bb1667ca52d69d
JonLopez22 <lopez.jay22@yahoo.com>
2026-05-06 16:14:33 -0400

    push all files to ex 2b more math scripts

---
+++ week-5/Assignments/Ex2B_MoreMathScripts/c_to_f.py
@@ -0,0 +1,5 @@
+# 2. How do you convert a temperature from Celsius to Fahrenheit?
+
+celsius = float(input("Enter temperature in Celsius: "))
+fahrenheit = (celsius * 9 / 5) + 32
+print(f"{celsius} degrees Celsius is equal to {fahrenheit:.2f} degrees Fahrenheit")
