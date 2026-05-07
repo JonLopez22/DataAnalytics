703bfc721cf0824550dcf5f6a0e5687becdef3d8
JonLopez22 <lopez.jay22@yahoo.com>
2026-05-07 13:19:57 -0400

    apply changes to files

--- week-5/Class_Notes/Class7_conversion.py
+++ week-5/Class_Notes/Class7_conversion.py
@@ -1,5 +1,10 @@
-# 2. How do you convert a temperature from Celsius to Fahrenheit?
+# Converting Celsius to Fahrenheit
 
+# Create the variable celsius
 celsius = float(input("Enter temperature in Celsius: "))
+
+# Now convert that variable (celsius) to Fahrenheit 
+
 fahrenheit = (celsius * 9 / 5) + 32
+
 print(f"{celsius} °C is equal to {fahrenheit:.2f} °F")
