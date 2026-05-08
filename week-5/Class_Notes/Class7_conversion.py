a9a041827bee4d0da7bdeda54d72baf8b3cc70b1
JonLopez22 <lopez.jay22@yahoo.com>
2026-05-08 15:19:00 -0400

    save files to their folders

--- week-5/Class_Notes/Class7_conversion.py
+++ week-5/Class_Notes/Class7_conversion.py
@@ -8,3 +8,20 @@ celsius = float(input("Enter temperature in Celsius: "))
 fahrenheit = (celsius * 9 / 5) + 32
 
 print(f"{celsius} °C is equal to {fahrenheit:.2f} °F")
+
+
+student = ('Alice', 20, 'Data Analytics', 3.5, True)
+           
+print(student)
+print(f"Name : {student[0]}")
+print(f"Age : {student[1]}")
+print(f"Major : {student[2]}")
+print(f"GPA : {student[3]}")
+print(f"Active : {student[4]}")
+print(f"Length : {len(student)}")
+
+# count (x)
+t = (1,2,2,3,2)
+print(t.count(2))
+
+print(t.index(2))
