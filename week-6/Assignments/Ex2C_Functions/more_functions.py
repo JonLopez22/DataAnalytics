d919d3c38101c8c7bb50678b645a5e60550208e6
Jonathan Lopez <lopez.jay22@yahoo.com>
2026-05-14 13:00:44 -0400

    push files to subfolder

---
+++ week-6/Assignments/Ex2C_Functions/more_functions.py
@@ -0,0 +1,9 @@
+# Displaying mail label
+def display_mailing_label(name, address, city, state, zip):
+    print(f"{name}")
+    print(f"{address}")
+    print(f"{city}, {state} {zip}")
+    print()
+
+display_mailing_label('Jonathan Lopez', '2244 Who St', 'New York', 'NY', '11385')
+display_mailing_label('Jane Smith', '456 Oak Ave', 'Brooklyn', 'NY', '11201')
